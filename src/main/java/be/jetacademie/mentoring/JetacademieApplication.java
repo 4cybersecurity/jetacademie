ed2a840eba92baa51fb84c64d9f2774417554d56
ahmetsimsek <snrsntrk2@gmail.com>
2024-09-30 14:15:10 +0200

    project created

---
+++ src/main/java/be/jetacademie/mentoring/JetacademieApplication.java
@@ -0,0 +1,13 @@
+package be.jetacademie.mentoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetacademieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetacademieApplication.class, args);
+    }
+
+}
